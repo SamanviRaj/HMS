07803571936ea4142874b50ca1e2c363df97881b
rthota7 <rthota7@dxc.com>
2024-03-19 22:06:43 +0530

    added new

---
+++ service-registery/src/main/java/com/hms/serviceregistery/ServiceRegisteryApplication.java
@@ -0,0 +1,13 @@
+package com.hms.serviceregistery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceRegisteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceRegisteryApplication.class, args);
+	}
+
+}
