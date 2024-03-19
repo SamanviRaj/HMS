417f0b5f546912dce807816b78779dab9801832e
rthota7 <rthota7@dxc.com>
2024-03-19 17:54:32 +0530

    initized the HMS with guest , reservations and room services

---
+++ reservation-service/src/test/java/com/hms/reservationservice/ReservationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hms.reservationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
